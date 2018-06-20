bfc6cf590d4681c0dbd2c380c8aecfe703e269ea
bry-an <bbyunis@gmail.com>
2018-06-19 23:13:16 -0500

    added search form

--- src/App.js
+++ src/App.js
@@ -21,13 +21,20 @@ import './App.css';
 },
 ];
 
+function isSearched(searchTerm) {
+  return function(item) {
+    return item.title.toLowerCase().includes(searchTerm.toLowerCase());
+  }
+}
 class App extends Component {
   constructor(props) {
     super(props);
     console.log(this);
     this.state = {
       list: list,
+      searchTerm: "",
     };
+    this.onSearchChange = this.onSearchChange.bind(this);
     this.onDismiss = this.onDismiss.bind(this);
   }
 
@@ -37,11 +44,20 @@ class App extends Component {
     this.setState({ list: updatedList });
   }
 
+  onSearchChange (event) {}
+  this.setState({ searchTerm: event.target.value})
+
 
   render() {
     return (
       <div className="App">
-        {this.state.list.map(item =>
+        <form>
+          <input
+            type = "text"
+            onChange = {this.onSearchChange}
+          />
+          </form>
+        {this.state.list.filter(isSearched(this.state.searchTerm)).map(item =>
             <div key={item.objectID}>
                 <span>
                   <a href={item.url}>{item.title}</a>
